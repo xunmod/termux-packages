98d48650256c71a2d5f93ffaf240c5ae722cfc3b
xunmod <116288337+xunmod@users.noreply.github.com>
2024-09-07 11:33:51 +0800

    bump(main/libusbmuxd): 2.1.0-p20240416

--- packages/libusbmuxd/build.sh
+++ packages/libusbmuxd/build.sh
@@ -2,12 +2,12 @@ TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org
 TERMUX_PKG_DESCRIPTION="A client library for applications to handle usbmux protocol connections with iOS devices"
 TERMUX_PKG_LICENSE="LGPL-2.1, GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-_COMMIT=f47c36f5bd2a653a3bd7fb1cf1d2c50b0e6193fb
-_COMMIT_DATE=20230430
-TERMUX_PKG_VERSION=2.0.2-p${_COMMIT_DATE}
+_COMMIT=a7f0543fb1ecb20ac7121c0fd77297200e0e43fc
+_COMMIT_DATE=20240416
+TERMUX_PKG_VERSION=2.1.0-p${_COMMIT_DATE}
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=git+https://github.com/libimobiledevice/libusbmuxd
-TERMUX_PKG_SHA256=c415b8859ccae494387c14dc9c297910843b16b5ba62aab15d51116137b513c4
+TERMUX_PKG_SHA256=67b69474735aea28e0a8c6c44999a17547da9e49a63fd2c156a08d103e7e9f92
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libimobiledevice-glue, libplist, libusb"
@@ -35,7 +35,7 @@ termux_step_post_get_source() {
 
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=6
+	local _SOVERSION=7
 
 	local e=$(sed -En 's/LIBUSBMUXD_SO_VERSION="?([0-9]+):([0-9]+):([0-9]+).*/\1-\3/p' \
 				configure.ac)
