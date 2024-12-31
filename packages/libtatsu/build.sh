85c08866cd6b2be8cf0a77680d22c028f073b5f3
xunmod <116288337+xunmod@users.noreply.github.com>
2024-12-31 14:09:54 +0000

    bump(main/libtatsu): 1.0.4

--- packages/libtatsu/build.sh
+++ packages/libtatsu/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
 TERMUX_PKG_DESCRIPTION="Library handling the communication with Apple's Tatsu Signing Server (TSS)"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.3
+TERMUX_PKG_MAINTAINER="@xunmod"
+TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libtatsu/releases/download/${TERMUX_PKG_VERSION}/libtatsu-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=4f69452d23e50e0ffbe844110e6ab6a900d080e051fbda3b7d595f679dee9bc5
+TERMUX_PKG_SHA256=08094e58364858360e1743648581d9bad055ba3b06e398c660e481ebe0ae20b3
 TERMUX_PKG_DEPENDS="libcurl (>= 7.0), libplist"
