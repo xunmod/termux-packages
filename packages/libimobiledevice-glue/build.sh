69af5c395c44aa9380f86c9c9c0fe9a74e4b425e
xunmod <116288337+xunmod@users.noreply.github.com>
2024-12-31 14:10:58 +0000

    bump(main/libimobiledevice-glue): 1.3.1

--- packages/libimobiledevice-glue/build.sh
+++ packages/libimobiledevice-glue/build.sh
@@ -1,8 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org/
 TERMUX_PKG_DESCRIPTION="Library with common code used by the libraries and tools around the libimobiledevice project"
 TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.3.0
+TERMUX_PKG_MAINTAINER="@xunmod"
+TERMUX_PKG_VERSION=1.3.1
+TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/${TERMUX_PKG_VERSION}/libimobiledevice-glue-${TERMUX_PKG_VERSION}.tar.bz2
-TERMUX_PKG_SHA256=96ec4eb2b1e217392149eafb2b5c3cd3e7110200f0e2bb5003c37d3ead7244ef
+TERMUX_PKG_SHA256=6e2849f221e6ab970566a115d42f3c20f8848e4d40c2ed61ac20dc85f40fa54f
 TERMUX_PKG_DEPENDS="libplist"
